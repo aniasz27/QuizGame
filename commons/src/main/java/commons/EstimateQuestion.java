c8270c591b255df05eb6cc81f7da8b50373b23e2
Andrei Drăgoi <a.dragoi@student.tudelft.nl>
2022-03-06 11:58:43 +0100

    Created question types

---
+++ commons/src/main/java/commons/EstimateQuestion.java
@@ -0,0 +1,71 @@
+package commons;
+
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public class EstimateQuestion extends Question {
+
+  private String question;
+  private Activity activity;
+
+  public EstimateQuestion() {
+    super(Type.ESTIMATE);
+  }
+
+  public EstimateQuestion(Activity activity) {
+    super(Type.ESTIMATE);
+    this.question = "How much does this activity take?";
+    this.activity = activity;
+  }
+
+
+  /**
+   * Returns how close answer is to correct value
+   *
+   * @param guessedValue value guessed by player
+   * @return float between 0 and 1:
+   * 0 for more than percentage% away from the correct answer
+   * 1 for correct answer
+   * linearly between them for answers within the percentage boundary
+   */
+  public float calculateHowClose(long guessedValue) {
+    float percentage = 20;
+    long correct = getActivity().consumption_in_wh;
+    if (guessedValue < (100 - percentage) / 100 * correct || (100 + percentage) / 100 * correct > guessedValue) {
+      return 0;
+    }
+    return (float) Math.abs(guessedValue - correct) / (correct * -(percentage / 100.0f)) + 1.0f;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public Activity getActivity() {
+    return activity;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof EstimateQuestion)) {
+      return false;
+    }
+    EstimateQuestion that = (EstimateQuestion) o;
+    return question.equals(that.question) && activity.equals(that.activity);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(question, activity);
+  }
+
+  @Override
+  public String toString() {
+    return "EstimateQuestion{"
+      + "question='" + question + '\''
+      + ", activity=" + activity + '}';
+  }
+}
---
+++ commons/src/main/java/commons/HowMuchQuestion.java
@@ -0,0 +1,89 @@
+package commons;
+
+import java.util.Random;
+
+@SuppressWarnings("unused")
+public class HowMuchQuestion extends Question {
+
+  private String question;
+  private Activity activity;
+  private long wrong1;
+  private long wrong2;
+
+  private final Random random;
+
+  public HowMuchQuestion() {
+    super(Type.HOWMUCH);
+    this.random = new Random();
+  }
+
+  public HowMuchQuestion(Activity activity) {
+    super(Type.HOWMUCH);
+    this.activity = activity;
+    this.random = new Random();
+    this.question = "How much energy does this activity use?";
+    computeWrong();
+  }
+
+  private void computeWrong() {
+    long correct = this.activity.consumption_in_wh;
+    int factor = 0;
+    while (factor < 10)  //make factor at least 10
+    {
+      factor = random.nextInt(50); // percentage to modify
+    }
+    int direction = random.nextInt(2); // 0 for going down, 1 for up
+
+    if (direction == 0) {
+      wrong1 = correct - correct * (100 - factor) / 100L;
+    } else {
+      wrong1 = correct + correct * (100 - factor) / 100L;
+    }
+
+    factor = 0;
+    while (factor < 10)  //make factor at least 10
+    {
+      factor = random.nextInt(50); // percentage to modify
+    }
+    direction = random.nextInt(2); // 0 for going down, 1 for up
+
+    if (direction == 0) {
+      wrong2 = correct - correct * (100 - factor) / 100L;
+    } else {
+      wrong2 = correct + correct * (100 - factor) / 100L;
+    }
+  }
+
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+
+  public Activity getActivity() {
+    return activity;
+  }
+
+  public void setActivity(Activity activity) {
+    this.activity = activity;
+  }
+
+  public long getWrong1() {
+    return wrong1;
+  }
+
+  public void setWrong1(long wrong1) {
+    this.wrong1 = wrong1;
+  }
+
+  public long getWrong2() {
+    return wrong2;
+  }
+
+  public void setWrong2(long wrong2) {
+    this.wrong2 = wrong2;
+  }
+}
