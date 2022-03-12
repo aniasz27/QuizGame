8c5d8cf642422399e7aa3d4a2b15606d8cc9e89a
Anna Magdalena Szymkowiak <A.M.Szymkowiak@student.tudelft.nl>
2022-03-12 16:54:31 +0100

    fix checkstyle2

--- client/src/main/java/client/scenes/GuessCtrl.java
+++ client/src/main/java/client/scenes/GuessCtrl.java
@@ -11,12 +11,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.text.Text;
 
 public class GuessCtrl extends QuestionCtrl implements Initializable {
   private EstimateQuestion question;
