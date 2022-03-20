03ea0ac7d7d9743c49cb4aca228b61431e80878e
Lucia Navarcikova <L.Navarcikova@student.tudelft.nl>
2022-03-20 14:46:27 +0100

    Added tests for class Score

---
+++ commons/src/test/java/commons/ScoreTest.java
@@ -0,0 +1,84 @@
+package commons;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+
+  static Score score;
+
+  @BeforeAll
+  public static void setUp() {
+    String player = "Username";
+    int points = 0;
+    score = new Score(player, points);
+  }
+
+  @Test
+  public void constructorTest() {
+    assertNotNull(score);
+  }
+
+  @Test
+  public void getPlayerTest() {
+    assertEquals("Username", score.getPlayer());
+  }
+
+  @Test
+  public void setPlayerTest() {
+    score.setPlayer("ME");
+    assertEquals("ME", score.getPlayer());
+    score.setPlayer("Username");
+  }
+
+  @Test
+  public void getPointsTest() {
+    assertEquals(0, score.getPoints());
+  }
+
+  @Test
+  public void setPointsTest() {
+    score.setPoints(100);
+    assertEquals(100, score.getPoints());
+    score.setPoints(0);
+  }
+
+  @Test
+  public void addPointsTest() {
+    score.addPoints(100);
+    assertEquals(100, score.getPoints());
+    score.setPoints(0);
+  }
+
+  @Test
+  public void equalsSameTest() {
+    assertEquals(score, score);
+  }
+
+  @Test
+  public void equalsSameButDifferentTest() {
+    String player = "Username";
+    int points = 0;
+    Score score1 = new Score(player, points);
+    assertEquals(score, score1);
+  }
+
+  @Test
+  public void equalsDifferentTest() {
+    String player = "Username";
+    int points = 100;
+    Score score1 = new Score(player, points);
+    assertFalse(score.equals(score1));
+  }
+
+  @Test
+  public void toStringTest() {
+    String result = "Score{id='0', player='Username', points='0'}";
+    assertEquals(result, score.toString());
+  }
+}
