160a412cc33204c86e4f959abeb7084248f46cc7
Anna Szymkowiak <a.m.szymkowiak@student.tudelft.nl>
2022-04-03 16:32:48 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/main/java/client/scenes/helpers/QuestionCtrl.java
+++ client/src/main/java/client/scenes/helpers/QuestionCtrl.java
@@ -310,19 +310,17 @@ public abstract class QuestionCtrl {
     mainCtrl.stopPointsTimer();
     submit.setDisable(true);
     answer.setDisable(true);
+int correct;
     if (question instanceof InsteadOfQuestion) {
-      int correct = ((InsteadOfQuestion) question).calculateHowClose(value);
-      if (correct > 0) {
-        return (correct + mainCtrl.getPointsOffset()) / 2;
-      } else {
-        return 0;
-      }
+      correct = ((InsteadOfQuestion) question).calculateHowClose(value);
     }
-    int correct = ((EstimateQuestion) question).calculateHowClose(value);
+else{
+correct = ((EstimateQuestion) question).calculateHowClose(value);
+}
     if (correct > 0) {
       return (correct + mainCtrl.getPointsOffset()) / 2;
-    } else {
-      return 0;
+    }
+     return 0;
     }
   }
 
